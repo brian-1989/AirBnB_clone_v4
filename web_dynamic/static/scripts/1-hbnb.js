fb64177f10f4155b5ad385971f4de194e492bde8
Brian Zapata <dzapata397@gmail.com>
2021-09-23 20:26:59 -0500

    validates the checkbox

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,17 @@
-$(document).ready(function(){
-    const dictAmenity = {};  
-    if ($('div.amenities li').is(':checked')){
-        dictAmenity[$(this).attr('data-id')] = $(this).attr('data-name');
-    } else {
-        delete dictAmenity[$(this).attr('data-id')]
-    }
-    $('div.amenities h4').map(dictAmenity, function(key, value) {
-        return value;
-    })
-    /* $('div.amenities h4').text(dictAmenity.$(this).data('id')); */
-})
+/* This script validates the checkbox, if the checkbox is checked display the name
+of the article. Otherwise, remove the name of the article */
+document.addEventListener('DOMContentLoaded', function () {
+	const dictAmenity = {};
+	$('input[type=checkbox]').change(
+		function () {
+			if ($(this).is(':checked')) {
+				dictAmenity[$(this).attr('data-id')] = $(this).attr('data-name');
+				console.log($(this).attr('data-id'));
+			} else {
+				delete dictAmenity[$(this).attr('data-id')];
+			}
+			console.log(dictAmenity);
+			$('.amenities h4').text(Object.values(dictAmenity).join(', '));
+		}
+	);
+});
