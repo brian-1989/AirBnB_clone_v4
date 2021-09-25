ce08216f5518eae723cbdc8f77beb57eadbd8144
Brian Zapata <dzapata397@gmail.com>
2021-09-25 14:58:12 -0500

    The console.log was removed

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -6,11 +6,9 @@ document.addEventListener('DOMContentLoaded', function () {
 		function () {
 			if ($(this).is(':checked')) {
 				dictAmenity[$(this).attr('data-id')] = $(this).attr('data-name');
-				console.log($(this).attr('data-id'));
 			} else {
 				delete dictAmenity[$(this).attr('data-id')];
 			}
-			console.log(dictAmenity);
 			$('.amenities h4').text(Object.values(dictAmenity).join(', '));
 		}
 	);
